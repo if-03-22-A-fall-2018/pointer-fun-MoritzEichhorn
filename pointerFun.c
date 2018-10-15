ac0d125abdef931f765575d25014c1a2af1d687e
MoritzEichhorn <moritz.eichhorn@outlook.com>
2018-10-15 17:54:05 +0200

    Added comment

--- pointerFun.c
+++ pointerFun.c
@@ -15,12 +15,12 @@
 #include <stdio.h>
 
 void print_integers(int int_value, int* int_pointer){
-  printf("Got an integer value %d and an address to an integer with value %p\n",int_value, (void*)int_pointer);
+  printf("Got an integer value %d and an address to an integer with value %d\n",int_value, *int_pointer);
 }
 
 void change_integers(int int_value, int* int_pointer){
   int_value = 3;
-  int_pointer = &int_value;
+  *int_pointer = 4;
 }
 
 int main(int argc, char const *argv[]) {
@@ -31,7 +31,7 @@ int main(int argc, char const *argv[]) {
   int_pointer = &int_value;
 
   print_integers(int_value, int_pointer);
-  change_integers(int_value, int_pointer); //nothing will change because the function is call by value, if I want to change the value I would have to to call the functoin by reference
+  change_integers(int_value, int_pointer);  // es ändern sich beide Werte auf 4, weil in "change_integers" der Wert, der dort gespeichert ist wo der pointer hinzeigt, geändert wird. Der Wert int_value wird daher in der main auch auf 4 geändert. Das zuweisen von 3 in change_integers macht genau gar nichts, weil int_value nicht per Reference übergeben wird.
   print_integers(int_value, int_pointer);
   return 0;
 }
